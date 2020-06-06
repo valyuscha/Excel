ace44211d8d0d5be6a635d030f78f7a8584f8dd4
Valentine <valyuscha2004@gmail.com>
2020-06-06 20:48:57 +0300

    Finish redux and table logic

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,3 +1,7 @@
+import {defaultTitle} from '@/constants'
+import {debounce} from '@core/utils'
+import {changeTitle} from '@/redux/actions'
+import {$} from '@core/dom'
 import {ExcelComponent} from '@core/ExcelComponent'
 
 export class Header extends ExcelComponent {
@@ -6,13 +10,19 @@ export class Header extends ExcelComponent {
   constructor($root, options) {
     super($root, {
       name: 'Header',
+      listeners: ['input'],
       ...options
     })
   }
 
+  prepare() {
+    this.onInput = debounce(this.onInput, 300)
+  }
+
   toHTML() {
+    const title = this.store.getState().title || defaultTitle
     return `
-      <input type="text" class="input" value="Новая таблица">
+      <input type="text" class="input" value="${title}">
       <div>
         <div class="button">
           <i class="material-icons">delete</i>
@@ -22,5 +32,10 @@ export class Header extends ExcelComponent {
         </div>
       </div>
     `
+  }
+
+  onInput(event) {
+    const $target = $(event.target)
+    this.$dispatch(changeTitle($target.text()))
   }
 }
